93473a758380f669d2d525a50bfc3951f3f73149
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2026-01-12 13:46:18 +0800

    修改

--- plugins/system/friend.js
+++ plugins/system/friend.js
@@ -11,7 +11,7 @@ export class friend extends plugin {
 
   async accept() {
     if (this.e.sub_type == "add" || this.e.sub_type == "single") {
-      if (cfg.other.autoFriend == 1) {
+      if (cfg.other.autoFriend == 1 || cfg.other.autoFriend == this.e.self_id) {
         logger.mark(`[自动同意][添加好友] ${this.e.user_id}`)
         await Bot.sleep(3000)
         this.e.approve(true)
